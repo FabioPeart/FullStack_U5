37f3e80844d0d9b26b15baac3e33ca3854186a1c
Ubuntu <vagrant@Base-Box>
2018-09-30 20:44:50 +0000

    Requirement 18 - Final Commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,8 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    @products_total = Product.count
+    @products_average_price = Product.average("price")
+    @products_stock_summed = Product.sum("stock_quantity")
   end
 end
